024066b4baf0f52f522c7a3d45682ded1681a619
m1m0zzz <contact.m1m0zzz@gmail.com>
2024-09-25 12:13:34 +0900

    update: package

--- packages/web-components/.storybook/main.ts
+++ packages/web-components/.storybook/main.ts
@@ -1,14 +1,5 @@
 import type { StorybookConfig } from '@storybook/web-components-vite'
 
-import { join, dirname } from 'path'
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
-}
 const config: StorybookConfig = {
   stories: [
     '../__stories__/**/*.mdx',
@@ -20,7 +11,7 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/web-components-vite'),
+    name: '@storybook/web-components-vite',
     options: {},
   },
 }
