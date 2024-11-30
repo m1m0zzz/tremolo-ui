edac9c3817f52e56025f5ed19f6618624fc21e2e
m1m0zzz <contact.m1m0zzz@gmail.com>
2024-12-01 03:27:37 +0900

    update: rollup config

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,43 +1,46 @@
+import path from 'path'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json' with { type: 'json' }
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: pkg.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      commonjs({
-        include: ['node_modules/**'],
-      }),
-      typescript({
-        tsconfig: './tsconfig.json',
-        exclude: ['**/__tests__/**', '**/__stories__/**'],
-      }),
-    ],
-    external: ['react', 'react-dom', '@emotion/react'],
-  },
-  {
-    input: 'dist/cjs/types/src/index.d.ts',
-    output: [{ file: 'dist/cjs/index.d.ts', format: 'cjs' }],
-    plugins: [dts()],
-  },
-  {
-    input: 'dist/esm/types/src/index.d.ts',
-    output: [{ file: 'dist/esm/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
-  },
-]
+/**
+ *
+ * @param {'cjs' | 'esm'} format
+ */
+function getConfig(format) {
+  const dir =
+    format == 'cjs' ? path.dirname(pkg.main) : path.dirname(pkg.module)
+  return [
+    {
+      input: 'src/index.ts',
+      output: [
+        {
+          dir: dir,
+          format: format,
+          sourcemap: true,
+        },
+      ],
+      plugins: [
+        commonjs({
+          include: ['node_modules/**'],
+        }),
+        typescript({
+          tsconfig: './tsconfig.json',
+          outDir: `dist/${format}`,
+          declarationDir: `dist/${format}/types`,
+          exclude: ['**/__tests__/**', '**/__stories__/**'],
+        }),
+      ],
+      external: ['react', 'react-dom', '@emotion/react'],
+    },
+    {
+      input: `dist/${format}/types/src/index.d.ts`,
+      output: [{ file: `dist/${format}/index.d.ts`, format: format }],
+      plugins: [dts()],
+    },
+  ]
+}
+
+export default [...getConfig('cjs'), ...getConfig('esm')]
