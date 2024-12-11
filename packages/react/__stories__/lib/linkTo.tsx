421b3f0a658509fa3cc61a4fb90e76cb4263b9bc
m1m0zzz <contact.m1m0zzz@gmail.com>
2024-12-11 16:26:49 +0900

    update: storybook docs

---
+++ packages/react/__stories__/lib/linkTo.tsx
@@ -0,0 +1,26 @@
+import { AnchorHTMLAttributes, ReactNode } from 'react'
+import { default as SbLinkTo } from '@storybook/addon-links/react'
+import { useTheme } from '@storybook/core/theming'
+
+interface Props {
+  kind?: string;
+  title?: string;
+  story?: string;
+  name?: string;
+  children?: ReactNode;
+}
+
+export function LinkTo({
+  story = 'docs',
+  children,
+  ...props
+}: Props & Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof Props>) {
+  const theme = useTheme()
+  return (
+    <SbLinkTo
+      story={story}
+      style={{ color: theme.brand.url }}
+      {...props}
+    >{children}</SbLinkTo>
+  )
+}
