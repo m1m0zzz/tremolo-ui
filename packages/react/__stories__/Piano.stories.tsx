4194a6206c61c2c6881eefc710a38b4f5f174277
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-01-11 19:22:23 +0900

    update: Piano component

--- packages/react/__stories__/Piano.stories.tsx
+++ packages/react/__stories__/Piano.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta } from '@storybook/react'
+import { noteNumber, noteName } from '@tremolo-ui/functions'
+import { useState } from 'react'
 
-import { noteNumber } from '@tremolo-ui/functions'
 import { Piano } from '../src/components/Piano'
 
 export default {
@@ -10,9 +11,34 @@ export default {
 } satisfies Meta<typeof Piano>
 
 export const Basic = () => {
+  const [first, setFirst] = useState(noteNumber('C3'))
+  const [last, setLast] = useState(noteNumber('B5'))
   return (
-    <Piano
-      noteRange={{ first: noteNumber('C3'), last: noteNumber('B5') }}
-    />
+    <div>
+      <div>
+        <label>first note:{' '}
+          <select onChange={(e) => setFirst(parseInt(e.target.value))}>
+            {[...Array(127)].map((_, i) => (
+              <option key={i} value={i} disabled={i >= last} selected={i === first}>
+                {noteName(i)}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>last note:{' '}
+          <select onChange={(e) => setLast(parseInt(e.target.value))}>
+          {[...Array(127)].map((_, i) => (
+            <option key={i} value={i} disabled={i <= first} selected={i === last}>
+              {noteName(i)}
+            </option>
+          ))}
+          </select>
+        </label>
+      </div>
+      <Piano
+        noteRange={{ first: first, last: last }}
+      />
+    </div>
   )
 }
--- packages/react/src/components/Piano/index.tsx
+++ packages/react/src/components/Piano/index.tsx
@@ -1,6 +1,20 @@
 import { css } from "@emotion/react"
+import { isNatural, NoteName, noteName } from "@tremolo-ui/functions"
 
-
+const pitchPositions: Record<NoteName, number> = {
+  C: 0,
+  'C#': 0.55,
+  D: 1,
+  'D#': 1.8,
+  E: 2,
+  F: 3,
+  'F#': 3.5,
+  G: 4,
+  'G#': 4.7,
+  A: 5,
+  'A#': 5.85,
+  B: 6,
+}
 
 interface Props {
   noteRange: { first: number, last: number }
@@ -13,7 +27,21 @@ export function Piano({
   playNote,
   stopNote,
 }: Props) {
-  // const parseNoteName =
+  const noteRangeArray = Array.from({ length: noteRange.last - noteRange.first + 1 }, (_, i) => i + noteRange.first)
+  const noteNameRangeArray = noteRangeArray.map(noteName)
+  console.log(noteRangeArray)
+  const noteWidth = 40
+
+  function notePosition(note: number) {
+    const cToB = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    const _noteName = noteName(note).slice(0, -1) as NoteName
+    const firstNoteName = noteName(noteRange.first).slice(0, -1) as NoteName
+    const pos = pitchPositions[_noteName] - pitchPositions[firstNoteName]
+    const octave = Math.floor((note - noteRange.first) / 12)
+    const octaveOffset = cToB.indexOf(firstNoteName) > cToB.indexOf(_noteName) ? 1 : 0
+    return pos * (noteWidth + 3) + (octave + octaveOffset) * 7 * (noteWidth + 3)
+  }
+
   return (
     <div
       className="tremolo-piano-wrapper"
@@ -29,22 +57,27 @@ export function Piano({
           flexDirection: 'row',
           height: 160,
           gap: 1,
+          position: 'relative',
         }}
       >
-      {['C', 'D', 'E', 'F', 'G', 'A', 'B'].map((note) => {
+      {noteRangeArray.map((note) => {
         return (
           <div
             key={note}
             className="tremolo-piano-note"
             css={css({
-              backgroundColor: 'white',
-              width: 40,
-              height: '100%',
+              position: 'absolute',
+              backgroundColor: isNatural(note) ? 'white' : '#333',
+              color: isNatural(note) ? 'black' : 'white',
+              left: notePosition(note),
+              width: isNatural(note) ? noteWidth : noteWidth * 0.65,
+              height: isNatural(note) ? '100%' : '60%',
               border: '1px solid #555',
               borderRadius: '0 0 8px 8px',
               cursor: 'pointer',
+              zIndex: isNatural(note) ? 1 : 2,
               '&:active': {
-                backgroundColor: '#ccc',
+                backgroundColor: isNatural(note) ? '#ccc' : '#666',
               }
             })}
             onPointerDown={() => {
@@ -62,19 +95,18 @@ export function Piano({
                 justifyContent: 'center',
                 alignItems: 'end',
                 height: '100%',
+                fontSize: '0.6rem',
+                textAlign: 'center',
               })}
             >
               <div
                 css={css({
-                  marginTop: 12,
-                  marginBottom: 12,
-                  backgroundColor: '#fff9',
-                  width: '1rem',
-                  fontSize: '0.75rem',
-                  textAlign: 'center',
+                  marginTop: 8,
+                  marginBottom: 8,
+                  padding: 4,
                   borderRadius: 4,
                 })}
-              >{note}</div>
+              >{noteName(note)}</div>
             </div>
           </div>
         )
