e99cc4d6ca040214ef11b2dd945f08f5055702f8
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-01-26 19:19:07 +0900

    add: WheelObserver

---
+++ packages/react/__stories__/WheelObserver.stories.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { WheelObserver } from '../src/components/WheelObserver'
+import { NumberInput } from '../src/components/NumberInput'
+import { toFixed } from '@tremolo-ui/functions'
+
+export default {
+  title: 'React/Base Components/WheelObserver',
+  component: WheelObserver,
+  tags: ['autodocs'],
+}
+
+export const Basic = () => {
+  const [count, setCount] = useState(0)
+  const [deltaY, setDeltaY] = useState(0)
+  const [scale, setScale] = useState(1)
+
+  return (
+    <div>
+      <WheelObserver
+        onWheel={(event) => {
+          event.preventDefault()
+          const { deltaY } = event
+          setCount((count) => toFixed(count + deltaY * scale, 2))
+          setDeltaY(deltaY)
+        }}
+        style={{
+          width: 'fit-content',
+          padding: '1rem',
+          marginBottom: '0.5rem',
+          border: '1px solid black'
+        }}
+      >
+        Wheel here
+      </WheelObserver>
+      scale: <NumberInput
+        value={scale}
+        min={0.1}
+        step={0.1}
+        width={10}
+        typeNumber
+        onChange={(value) => {
+          setScale(value)
+        }}
+        style={{
+          width: 80,
+          marginRight: '0.5rem'
+        }}
+      />
+      <button type="button" onClick={() => setCount(0)}>Reset count</button>
+      <div>count: {count}</div>
+      <div>deltaY: {deltaY}</div>
+    </div>
+  )
+}
---
+++ packages/react/src/components/WheelObserver/index.tsx
@@ -0,0 +1,52 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react"
+import { useRefCallbackEvent } from "../../hooks/useRefCallbackEvent"
+
+/**
+ * @example
+ * <WheelObserver
+ *   onWheel={(event) => {
+ *     event.preventDefault()
+ *     console.log(event.deltaY)
+ *   }
+ * >
+ *   <div>Wheel here</div>
+ * </WheelObserver>
+ */
+
+interface Props<T extends ElementType> {
+  /**
+   * React.ElementType
+   * @default div
+   */
+  as?: T
+
+  children?: ReactNode
+  onWheel?: (event: WheelEvent) => void
+}
+
+export function WheelObserver<T extends ElementType = 'div'>(
+  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>
+) {
+  const {
+    children,
+    onWheel,
+    as: Component = 'div',
+    ...attributes
+  } = props
+
+  const wheelRefCallback = useRefCallbackEvent(
+    'wheel',
+    (event) => {
+      if (onWheel) onWheel(event)
+    },
+    { passive: false },
+    [onWheel]
+  )
+
+  return (
+    <Component
+      ref={wheelRefCallback}
+      {...attributes}
+    >{children}</Component>
+  )
+}
