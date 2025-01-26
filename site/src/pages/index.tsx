050caa2aef0de4aea477c2ec12dc54137bf9d4b2
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-01-27 02:05:54 +0900

    docs: update i18n

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -18,7 +18,11 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">
+          <Translate id="homepage.tagline">
+            {siteConfig.tagline}
+          </Translate>
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
