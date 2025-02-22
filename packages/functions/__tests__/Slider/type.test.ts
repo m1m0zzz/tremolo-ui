68da2ca046be6b9fb24142840b8446af499eb266
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-02-23 00:17:47 +0900

    fix: rename generateOptionsList in functions

--- packages/functions/__tests__/Slider/type.test.ts
+++ packages/functions/__tests__/Slider/type.test.ts
@@ -1,30 +1,30 @@
-import { parseScaleOrderList } from '../../src/components/Slider/type'
+import { generateOptionsList } from '../../src/components/Slider/type'
 
 describe('unit', () => {
-  test('parseScaleOrderList()', () => {
-    expect(parseScaleOrderList([10, 'mark-number'], 4, 30, 1)).toStrictEqual([
-      { at: 10, type: 'mark-number' },
-      { at: 20, type: 'mark-number' },
-      { at: 30, type: 'mark-number' },
+  test('generateOptionsList()', () => {
+    expect(generateOptionsList([10, 'mark-number'], 4, 30, 1)).toStrictEqual([
+      { value: 10, type: 'mark-number' },
+      { value: 20, type: 'mark-number' },
+      { value: 30, type: 'mark-number' },
     ])
-    expect(parseScaleOrderList(['step', 'number'], -10, 11, 4)).toStrictEqual([
-      { at: -8, type: 'number' },
-      { at: -4, type: 'number' },
-      { at: 0, type: 'number' },
-      { at: 4, type: 'number' },
-      { at: 8, type: 'number' },
+    expect(generateOptionsList(['step', 'number'], -10, 11, 4)).toStrictEqual([
+      { value: -8, type: 'number' },
+      { value: -4, type: 'number' },
+      { value: 0, type: 'number' },
+      { value: 4, type: 'number' },
+      { value: 8, type: 'number' },
     ])
     expect(
-      parseScaleOrderList(['step', 'number'], -6.6, -3.3, 3.3),
+      generateOptionsList(['step', 'number'], -6.6, -3.3, 3.3),
     ).toStrictEqual([
-      { at: -6.6, type: 'number' },
-      { at: -3.3, type: 'number' },
+      { value: -6.6, type: 'number' },
+      { value: -3.3, type: 'number' },
     ])
     expect(
-      parseScaleOrderList(['step', 'number'], 410, 500, 400),
+      generateOptionsList(['step', 'number'], 410, 500, 400),
     ).toStrictEqual([])
-    expect(parseScaleOrderList(['step', 'number'], 10, 400, 400)).toStrictEqual(
-      [{ at: 400, type: 'number' }],
+    expect(generateOptionsList(['step', 'number'], 10, 400, 400)).toStrictEqual(
+      [{ value: 400, type: 'number' }],
     )
   })
 })
