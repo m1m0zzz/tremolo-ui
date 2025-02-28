a5e23ab8f5745aaf17a627de91f1cd66d3d3f695
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-03-01 05:51:10 +0900

    fix: storybook config

--- packages/react/.storybook/preview.ts
+++ packages/react/.storybook/preview.ts
@@ -5,14 +5,10 @@ import '../src/index' // import css without build
 const preview: Preview = {
   parameters: {
     controls: {
-      // exclude: ['value', 'children'],
       matchers: {
         color: /(background|bg|color)$/i,
         date: /Date$/i,
       },
-    },
-    actions: {
-      argTypesRegex: '^on[A-Z].*', // TODO: is not working?
     },
   },
   tags: ['autodocs'],
