ebc3fbe37f9456bf6a05c0678b8cd5ac9ca1643a
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-03-08 08:46:07 +0900

    add: useAnimationFrame hooks

---
+++ packages/react/src/hooks/useAnimationFrame.ts
@@ -0,0 +1,18 @@
+import { DependencyList, useCallback, useEffect, useRef } from 'react'
+
+export const useAnimationFrame = (
+  callback = () => {},
+  deps: DependencyList = [],
+) => {
+  const reqIdRef = useRef(-1)
+  const loop = useCallback(() => {
+    reqIdRef.current = requestAnimationFrame(loop)
+    callback()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [callback, ...deps])
+
+  useEffect(() => {
+    reqIdRef.current = requestAnimationFrame(loop)
+    return () => cancelAnimationFrame(reqIdRef.current)
+  }, [loop])
+}
