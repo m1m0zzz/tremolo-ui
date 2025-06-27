f443c3b3587a35fc2658b0911b6f541cdecf936d
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-06-28 04:45:48 +0900

    fix(docs): Piano example

--- site/examples/components/piano/basic.tsx
+++ site/examples/components/piano/basic.tsx
@@ -9,16 +9,14 @@ function App() {
   return (
     <div
       style={{
-        width: '100%',
-        height: 160,
-        margin: 'auto',
-        padding: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Piano
         noteRange={{ first: noteNumber('C3'), last: noteNumber('B4') }}
         keyboardShortcuts={SHORTCUTS.HOME_ROW}
-        fill
         // Notice: need optional chaining (?.)
         label={(_, i) => SHORTCUTS.HOME_ROW.keys[i]?.toUpperCase()}
       />
