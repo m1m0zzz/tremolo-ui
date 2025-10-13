1fa549a3611601157cc9cf8718cee9b2bf64118e
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-10-14 03:21:14 +0900

    fix

--- site/examples/components/piano/basic.tsx
+++ site/examples/components/piano/basic.tsx
@@ -1,11 +1,14 @@
 // expand begin
-import { noteNumber } from '@tremolo-ui/functions'
+import { noteName, noteNumber } from '@tremolo-ui/functions'
 import { Piano, SHORTCUTS } from '@tremolo-ui/react'
+import * as Tone from 'tone'
 
 import '@tremolo-ui/react/styles/index.css'
 // expand end
 
 function App() {
+  const synth = new Tone.PolySynth({ volume: -6 }).toDestination()
+
   return (
     <div
       style={{
@@ -17,6 +20,12 @@ function App() {
       <Piano
         noteRange={{ first: noteNumber('C3'), last: noteNumber('B4') }}
         keyboardShortcuts={SHORTCUTS.HOME_ROW}
+        onPlayNote={(noteNumber) => {
+          synth.triggerAttack(noteName(noteNumber))
+        }}
+        onStopNote={(noteNumber) => {
+          synth.triggerRelease(noteName(noteNumber))
+        }}
         // Notice: need optional chaining (?.)
         label={(_, i) => SHORTCUTS.HOME_ROW.keys[i]?.toUpperCase()}
       />
